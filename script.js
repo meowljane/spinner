98859b4f4ea3fd3ba59f24e7544b967c3a1c703d
meowljane <69661367+meowljane@users.noreply.github.com>
2020-08-31 15:45:18 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("load", function(){
+  
+  var speedInput = document.querySelector("input[name=speed]")
+  
+  speedInput.addEventListener("change", function()
+{document.getElementById("img").style.cssText = "-webkit-animation-duration: " + speedInput.value + "s;";
+
+  });
+
+});
